0b45bd5701bb4d4c230f7d63dcd6e614bb58e23d
konarsada <sadabratak@gmail.com>
2022-05-24 23:59:19 +0530

    BrowserRouter, Routes, Route, Link introduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Routes, Route, Link} from "react-router-dom"
+
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+
+export default function App() {
+    return (
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
+
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+
+                <Route path="/about" element={<About />} />
+
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </div>
+    )
 }
-
-export default App;
