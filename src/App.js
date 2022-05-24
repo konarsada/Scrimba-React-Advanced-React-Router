aa735ec76b7e9a311b1c55a75b4aa9f5870bb794
konarsada <sadabratak@gmail.com>
2022-05-25 02:23:32 +0530

    React Router useParams

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Header from "./components/Header"
-import Footer from "./components/Footer"
 import Home from "./pages/Home"
-import Profile from "./pages/profile/Profile"
+import ServicesList from "./pages/services/ServicesList"
+import ServiceDetail from "./pages/services/ServiceDetail"
 
 import {Switch, Route} from "react-router-dom"
 
@@ -15,12 +15,13 @@ function App() {
                 <Route exact path="/">
                     <Home />
                 </Route>
-                <Route path="/profile">
-                    <Profile/>
+                <Route exact path="/services" >
+                    <ServicesList />
+                </Route>
+                <Route path="/services/:serviceId" >
+                    <ServiceDetail />
                 </Route>
             </Switch>
-            
-            <Footer />
         </div>
     )
 }
